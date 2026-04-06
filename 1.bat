3d21c8f8a2581c3b810731a02ef582a91438bd4f
scmoonkr <scmoonkr@naver.com>
2026-04-06 10:50:03 +0900

    test ok

---
+++ 1.bat
@@ -0,0 +1,6 @@
+node src\index.js detailLinkClassAll 55890
+node src\index.js detailLinkClassAll 2105
+node src\index.js detailLinkClassAll 987
+node src\index.js detailLinkClassAll 8257
+node src\index.js detailLinkClassAll 2551
+
---
+++ 2.bat
@@ -0,0 +1,19 @@
+node src\index.js detailLinkClassAll 170
+node src\index.js detailLinkClassAll 798
+node src\index.js detailLinkClassAll 1
+
+
+# node src/index.js listCategoryAll 55889 5000 
+# node src/index.js listCategoryAll 1108 5000 
+# node src/index.js listCategoryAll 13789 5000 
+# node src/index.js listCategoryAll 1196 5000 
+# node src/index.js listCategoryAll 74 5000 
+# node src/index.js listCategoryAll 517 5000 
+# node src/index.js listCategoryAll 1322 5000 
+# node src/index.js listCategoryAll 656 5000 
+# node src/index.js listCategoryAll 336 5000 
+# node src/index.js listCategoryAll 112011 5000 
+# node src/index.js listCategoryAll 1237 5000 
+# node src/index.js listCategoryAll 2030 5000 
+# node src/index.js listCategoryAll 1137 5000 
+# node src/index.js listCategoryAll 351 5000 
